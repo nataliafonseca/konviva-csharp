4588c8ba9b28581a08d3ae0e7787ec03db5de7f7
Natalia Braga da Fonseca - NFO <natalia.fonseca@iteris.com.br>
2022-05-24 15:02:09 -0300

    feat: desafio 1

---
+++ Desafio1/Desafio1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Informe o primeiro número:");
+double numA = double.Parse(Console.ReadLine());
+Console.WriteLine("Informe o segundo número:");
+double numB = double.Parse(Console.ReadLine());
+Console.WriteLine("Informe o terceiro número:");
+double numC = double.Parse(Console.ReadLine());
+
+Console.WriteLine("\nO produto é: " + (numA * numB * numC));
