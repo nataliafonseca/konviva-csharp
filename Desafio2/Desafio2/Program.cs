df621e2e900f761e9ec2091ff966d44a4949ad38
Natalia Braga da Fonseca - NFO <natalia.fonseca@iteris.com.br>
2022-05-24 15:21:39 -0300

    feat: desafio 2

---
+++ Desafio2/Desafio2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Crie uma nova classe chamada Usuario.
+// Deve conter o nome e o e-mail do usuário.
+// Crie um método chamado ObterLogin, que deve retornar a parte anterior ao @ no e-mail:
+// Exemplo: "douglas.pereira@iteris.com.br", deve retornar somente "douglas.pereira";
+// A Lógica de extração da primeira parte do e-mail deve estar dentro da classe Usuario, já que ela é a responsável pela informação;
+// O arquivo Program.cs deve ser responsável apenas por:
+// Instanciar o Objeto;
+// Chamar o método ObterLogin;
+using Desafio2;
+
+Usuario user1 = new("Natália Fonseca", "natalia.fonseca@iteris.com.br");
+
+Console.WriteLine("O usuário é: " + user1.ObterLogin());
---
+++ Desafio2/Desafio2/Usuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio2
+{
+    public class Usuario
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public Usuario(string nome, string email)
+        {
+            this.Nome = nome;
+            this.Email = email;
+        }
+
+        public string ObterLogin() {
+            return this.Email.Split("@")[0];
+        }
+    }
+}
